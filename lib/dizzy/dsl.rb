d8abfb34c17873eb941afb5e31e2e16891223d25
Brian Lauber <constructible.truth@gmail.com>
2015-01-05 20:14:27 -0500

    Verbose syntax appears to be working

--- lib/dizzy/dsl.rb
+++ lib/dizzy/dsl.rb
@@ -4,13 +4,33 @@ require 'dizzy/rule'
 
 module Dizzy::DSL
 
-  def di(name)
+  class RuleContext
+
+    attr_reader :rule
+
+    def initialize(rule = ::Dizzy::Rule.new)
+      @rule = rule
+    end
+
+    def init(&block)
+      rule.init_proc = block
+    end
+
+    def wire(&block)
+      rule.wire_proc = block
+    end
+  end
+
+
+  def di(name, &block)
     ___method_name   = name.to_sym
     ___variable_name = "@#{name}".to_sym
 
-    rule = Dizzy::Rule.new
+    rc   = ::Dizzy::DSL::RuleContext.new
+    rc.instance_exec(&block)
+    rule = rc.rule
 
-    define_method ___method_name do 
+    define_method ___method_name do
       unless instance_variable_defined? ___variable_name
         value = self.instance_exec(&rule.init_proc)
         instance_variable_set(___variable_name, value)
@@ -21,5 +41,8 @@ module Dizzy::DSL
     end
 
   end
+
+
+
 
 end
