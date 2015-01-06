e9006de62f2295e8a12bc70cb99254b2c8fd0d52
Brian Lauber <constructible.truth@gmail.com>
2015-01-05 20:41:44 -0500

    Added an over-simplified unit test

---
+++ spec/dizzy/dsl_spec.rb
@@ -0,0 +1,64 @@
+
+require 'dizzy/dsl'
+
+
+class Foo
+  attr_accessor :name, :bar
+
+  def initialize(name = nil)
+    @name = name
+  end
+end
+
+
+class Bar
+  attr_accessor :name, :foo
+
+  def initialize(name = nil)
+    @name = name
+  end
+end
+
+
+
+describe Dizzy::DSL do 
+
+  # TODO: Split this into many many many more tests
+  it "works" do
+
+    frame = Class.new do
+      extend Dizzy::DSL
+
+      di :foo do
+        init do
+          Foo.new("foo")
+        end
+
+        wire do |foo|
+          foo.bar = bar
+        end
+      end
+
+      di :bar do 
+        init do
+          Bar.new("bar")
+        end
+        wire do |bar|
+          bar.foo = foo
+        end
+      end
+    end
+
+
+    f = frame.new
+
+    expect(f.foo.name).to eq "foo"
+    expect(f.bar.name).to eq "bar"
+
+    expect(f.foo.bar).to equal f.bar
+    expect(f.bar.foo).to equal f.foo
+
+  end
+
+end
+
